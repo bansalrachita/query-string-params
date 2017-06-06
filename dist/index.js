fe02ed464f9c9d5e06ec063a003c2d495367e5e7
Rachita Bansal <rbansal@federatedwireless.com>
2017-06-05 21:29:18 -0400

    recompiling the code

--- dist/index.js
+++ dist/index.js
@@ -13,4 +13,6 @@ var _urlToList = require("./urlToList");
 
 exports.propertyToUrl = _propertyToUrl.propertyToUrl;
 exports.urlToList = _urlToList.urlToList;
-exports.urlToProperty = _urlToProperty.urlToProperty;
+exports.urlToProperty = _urlToProperty.urlToProperty; /**
+                                                       * Created by rachitabansal on 6/5/17.
+                                                       */
