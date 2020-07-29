ea951f8f2e4ec715fb4dff84afa794eb2077248d
Rachita <rachita.gemm30@gmail.com>
2020-07-28 23:40:15 -0700

    update package version.

--- dist/urlToList.js
+++ dist/urlToList.js
@@ -15,11 +15,9 @@ var urlToList = exports.urlToList = function urlToList(queryString) {
     var result = [];
 
     if (queryString.length > 0) {
-
         var queryArray = queryString.substring(1).split("&");
 
         for (var i in queryArray) {
-
             var query = queryArray[i].split("=");
             var key = query[0];
             var obj = {};
@@ -27,11 +25,7 @@ var urlToList = exports.urlToList = function urlToList(queryString) {
             obj[key] = query[1].split(",") || [];
 
             for (var _i = 0; _i < obj[key].length; _i++) {
-
-                if (obj[key][_i].includes(encodeURIComponent("&"))) {
-
-                    obj[key][_i] = obj[key][_i].split(encodeURIComponent("&")).join(decodeURIComponent(encodeURIComponent("&")));
-                }
+                obj[key][_i] = decodeURIComponent(obj[key][_i]);
             }
 
             result.push(obj);
--- dist/urlToProperty.js
+++ dist/urlToProperty.js
@@ -15,24 +15,21 @@ var urlToProperty = exports.urlToProperty = function urlToProperty(queryString) 
     var result = {};
 
     if (queryString.length > 0) {
-
         var queryArray = queryString.substring(1).split("&");
 
         for (var i in queryArray) {
-
             var query = queryArray[i].split("=");
             var key = query[0];
 
             result[key] = query[1].split(",") || [];
 
             for (var _i = 0; _i < result[key].length; _i++) {
-
-                if (result[key][_i].includes(encodeURIComponent("&"))) {
-
-                    result[key][_i] = result[key][_i].split(encodeURIComponent("&")).join(decodeURIComponent(encodeURIComponent("&")));
-                }
+                result[key][_i] = decodeURIComponent(result[key][_i]);
             }
         }
     }
     return result;
 };
+
+// const result = urlToProperty("?a=x%26,y%26,z&b=z%20h");
+// console.log(result);
